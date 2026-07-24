96c21614a6fa3949c7c045634802a36861938a60
cbohan <connor.bohan@gmail.com>
2026-07-24 09:59:17 -0700

    Create 3 different levels

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Main Menu")]
+    [SerializeField] private CanvasGroup _mainMenuCanvasGroup;
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _creditsButton;
     [SerializeField] private int _gameSceneIndex = 1;
 
+    [Header("Level Select")]
+    [SerializeField] private CanvasGroup _levelSelectCanvasGroup;
+    
     private void Awake()
     {
         _playButton.onClick.AddListener(Play);

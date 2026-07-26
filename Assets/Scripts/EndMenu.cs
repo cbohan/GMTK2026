ab5212b37dd5d52e1a0e8faad12c0b5f0f7042ef
PusherCoder <bendegn@gmail.com>
2026-07-25 19:58:45 -0700

    It can now be played from start to finish, in a sense

---
+++ Assets/Scripts/EndMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndMenu : MonoBehaviour
+{
+    [SerializeField] private Button _replayButton;
+    [SerializeField] private TMP_Text _finalScore;
+    
+    private void Awake()
+    {
+        _finalScore.text = $"Congratulations! You ended with {FollowerTracker.Instance.followerCount} followers!";
+        _replayButton.onClick.AddListener(Replay);
+    }
+
+    private void Replay()
+    {
+        FollowerTracker.Instance.followerCount = 1000;
+        SceneManager.LoadScene("Level1_DownsScene");
+        return;
+    }
+}
